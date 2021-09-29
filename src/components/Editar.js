9dbae4b133f176af1ef9274f904f6ef373dc2859
Guido Magi <guido_magi@hotmail.com>
2021-09-29 03:24:38 -0300

    Se crea el ABM con el tutorial

---
+++ src/components/Editar.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import Api from "../servicios/Api";
+
+
+class Editar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            datosCargados: false,
+            empleado:[]             
+         }
+    }
+
+    cambioValor = (e)=> {
+        const state=this.state.empleado;
+
+        state[e.target.name]=e.target.value;
+        this.setState({empleado:state});
+    }
+
+    enviarDatos = (e) =>{
+        e.preventDefault();
+        console.log("Formulario enviado");
+        const{id,nombre,correo}= this.state.empleado;
+        console.log(id);
+        console.log(nombre);
+        console.log(correo);
+
+        var datosEnviar={id:id, nombre:nombre, correo:correo}
+
+        fetch(Api+"?actualizar=1",{
+            method:"POST",
+            body:JSON.stringify(datosEnviar)
+
+        })
+        .then(respuesta => respuesta.json())
+        .then((datosRespuesta) => {
+            console.log(datosRespuesta);
+            this.props.history.push("/");
+        })
+        .catch(console.log)
+    }
+
+    componentDidMount(){
+        console.log(this.props.match.params.id);
+
+        fetch(Api+"?consultar="+this.props.match.params.id)
+            .then(respuesta => respuesta.json())
+            .then((datosRespuesta) => {
+                console.log(datosRespuesta);
+                this.setState({ 
+                    datosCargados: true,
+                    empleado: datosRespuesta[0]
+                })
+            })
+            .catch(console.log)
+    }
+
+    render() { 
+
+        const { datosCargados, empleado } = this.state
+        if (!datosCargados) { return (<div>Cargando...</div>); }
+        else {
+
+            return ( 
+            <div className="card">
+                <div className="card-header">
+                    Editar Empleados
+                </div>
+                <div className="card-body">
+                   
+
+                    <div className="form-group">
+                      <label htmlFor="">Clave</label>
+                      <input type="text" readOnly className="form-control" value= {empleado.id} id="id" aria-describedby="helpId" placeholder=""/>
+                      <small id="helpId" className="form-text text-muted">Clave Id</small>
+                    </div>
+
+                    <form onSubmit={this.enviarDatos} >
+                        <div className="form-group">
+                        <label htmlFor="">Nombre:</label>
+                        <input type="text" name="nombre" onChange={this.cambioValor} value={empleado.nombre} id="nombre" className="form-control" placeholder="" aria-describedby="helpId"/>
+                        <small id="helpId" className="text-muted">Nombre del Empleado</small>
+                        </div>
+
+                        <div className="form-group">
+                        <label htmlFor="">Correo:</label>
+                        <input type="text" name="correo" id="Correo" onChange={this.cambioValor} value={empleado.correo} className="form-control" placeholder="" aria-describedby="helpId"/>
+                        <small id="helpId" className="text-muted">Ingrese Correo</small>
+                        </div>
+
+                        <div className="btn-group" role="group" aria-label="">
+                            <button type="submit" className="btn btn-success">Actualizar</button>
+                            <Link to={"/"} className="btn btn-cancel">Cancelar</Link>
+                        </div>
+                    </form>
+                </div>
+                <div className="card-footer text-muted">
+                    
+                </div>
+            </div> );
+        }
+    }
+}
+ 
+export default Editar;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
