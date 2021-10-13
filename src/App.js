cadfc73d1646bd84cbe013f2b8f5f0a6fe847290
Guido Magi <guido_magi@hotmail.com>
2021-10-12 21:58:29 -0300

    Se sube el ABM y el Loguin funcionando

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-//import Login from "./components/Login";
-//import React, { useState } from 'react';
+import Login from "./components/Login";
+import React, { useState } from 'react';
 
 
-
+import Estacionamiento from "./components/Estacionamiento";
+import Abm from "./components/Abm";
 import Listar from "./components/Listar";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import { Link } from "react-router-dom";
@@ -11,43 +12,36 @@ import Editar from "./components/Editar";
 
 
 function App() {
+const [idRol, setIdRol] = useState(false);
+const acceder = (estado) => {
+     setIdRol(estado)
+   }
+
+   console.log("idrol",idRol);  
+     const project =() =>{
+       switch(idRol){
+         case 1: return <Abm/>;
+         case 2: return <Estacionamiento/>;
+
+         default: return <h1>Error...</h1>
+       }
+     } 
+
+  
+
 
   return (
-    <Router>
-      <nav className="navbar navbar-expand navbar-light bg-light">
-        <div className="nav navbar-nav">
+    
 
-          <Link className="nav-item nav-link active" to={"/"}>ABM de Estacionamientos</Link>
+      <>
+     {
+       (idRol) 
+       ? project() 
+       : <Login acceder={acceder} />
+     }
+     </>
 
-        </div>
-      </nav>
-
-      <div className="container">
-
-
-        <Route exact path="/" component={Listar}></Route>
-        <Route path="/Crear" component={Crear}></Route>
-        <Route path="/Editar/:id" component={Editar}></Route>
-      </div>
-    </Router>
   );
-
-
-  // const [idRol, setIdRol] = useState(false);
-
-  // const acceder = (estado) => {
-  //   setIdRol(estado)
-  // }
-
-  // console.log("idrol",idRol);  
-  //   const project =() =>{
-  //     switch(idRol){
-  //       case 1: return <Abm/>;
-  //       case 2: return <Estacionamiento/>;
-
-  //       default: return <h1>Error...</h1>
-  //     }
-  //   }  
 
 
   //   return (
