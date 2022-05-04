16fb67ebc41912876f74048129f9dcd06b4547c0
Guido Magi <guido_magi@hotmail.com>
2022-05-04 17:01:15 -0300

    diseño VerEstacionamiento

--- src/components/VerEstacionamiento.js
+++ src/components/VerEstacionamiento.js
@@ -1,4 +1,3 @@
-
 import { Link } from "react-router-dom";
 import React from 'react';
 import Api from "../servicios/Api";
@@ -12,20 +11,22 @@ class VerEstacionamiento extends React.Component {
         this.state = {
             datosCargados: false,
             estacionamiento: [],
+            playadeestacionamiento: [],
             estacionado: ""
         }
     }
-    
+
     cambioValor = (e) => {
         const state = this.state.estacionamiento;
+
         state[e.target.name] = e.target.value;
         this.setState({ estacionamiento: state });
     }
 
     enviarDatos = (e) => {
         e.preventDefault();
-        //console.log("Formulario enviado");
-        const { idPlayaDeEstacionamiento, nombrePlayaDeEstacionamiento, ubicacion, observaciones, mapa, lugaresLibres} = this.state.estacionamiento;
+        console.log("Formulario enviado");
+        const { idPlayaDeEstacionamiento, nombrePlayaDeEstacionamiento, ubicacion, observaciones, mapa, lugaresLibres } = this.state.estacionamiento;
 
 
         var datosEnviar = { idPlayaDeEstacionamiento: idPlayaDeEstacionamiento, nombrePlayaDeEstacionamiento: nombrePlayaDeEstacionamiento, ubicacion: ubicacion, observaciones: observaciones, mapa: mapa, lugaresLibres: lugaresLibres }
@@ -33,20 +34,21 @@ class VerEstacionamiento extends React.Component {
         fetch(Api + "?actualizar=1", {
             method: "POST",
             body: JSON.stringify(datosEnviar)
+
         })
             .then(respuesta => respuesta.json())
             .then((datosRespuesta) => {
-                //console.log(datosRespuesta);
+                console.log(datosRespuesta);
                 this.props.history.push("/");
             })
-            //.catch(console.log)
+            .catch(console.log)
     }
 
     componentDidMount() {
         fetch(Api + "?consultar=" + this.props.match.params.id)
             .then(respuesta => respuesta.json())
             .then((datosRespuesta) => {
-                //console.log(datosRespuesta);
+                console.log(datosRespuesta);
                 this.setState({
                     datosCargados: true,
                     estacionamiento: datosRespuesta[0]
@@ -56,20 +58,20 @@ class VerEstacionamiento extends React.Component {
     }
 
     estacionar = (idPlayaDeEstacionamiento, idUsuario) => {
-        //console.log("IdUSUARIO:", idUsuario);
+        console.log("IdUSUARIO:", idUsuario);
         fetch(ApiEstacionar + "?estacionar=" + idPlayaDeEstacionamiento + "&idUsuario=" + idUsuario)
             .then(response => response.json()
-            .then(data => {
-                
-                //console.log(data.data);
-                this.cargarDatos();
-                data.data === "estacionado" ? (alert("Te has estacionado")) 
-                : data.data === "ya_estacionado" ? (alert ("Ya estabas estacionado con anterioridad, desestacione y vuelva a intentar"))
-                : (alert ("El estacionamiento se encuentra cerrado"));
-                //console.log(data.data);
-            }))
+                .then(data => {
+
+                    console.log(data.data);
+                    this.cargarDatos();
+                    data.data === "estacionado" ? (alert("Te has estacionado"))
+                        : data.data === "ya_estacionado" ? (alert("Ya estabas estacionado con anterioridad, desestacione y vuelva a intentar"))
+                            : (alert("El estacionamiento se encuentra cerrado"));
+                    console.log(data.data);
+                }))
             .catch(console.log)
-            //alert('Usted se ha estacionado.');
+        //alert('Usted se ha estacionado.');
     }
 
 
@@ -79,67 +81,57 @@ class VerEstacionamiento extends React.Component {
         fetch(Api)
             .then(respuesta => respuesta.json())
             .then((datosRespuesta) => {
-                //console.log(datosRespuesta);
+                console.log(datosRespuesta);
                 this.setState({ datosCargados: true, playadeestacionamiento: datosRespuesta, estacionado: true });
             })
             .catch(console.log)
 
-            
+
     }
 
     render() {
-        const { datosCargados, estacionamiento } = this.state
+        const { datosCargados, estacionamiento, playadeestacionamiento } = this.state
         if (!datosCargados) { return (<div>Cargando...</div>); }
         else {
 
             return (
-                <div className="card">
 
-                    <div className="card-body">
+                <div className="tbl-header">
+                    <table cellPadding="0" cellSpacing="0" border="0">
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Ubicacion</th>
+                                <th>Lugares Libres</th>
+                                <th>Observaciones</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                    </table>
+                    <div className="tbl-content">
+                        <table cellPadding="0" cellSpacing="0" border="0">
+                            <tbody>
+                                <tr>
+                                    <td>{estacionamiento.nombrePlayaDeEstacionamiento}</td>
+                                    <td>{estacionamiento.ubicacion}</td>
+                                    <td>{estacionamiento.lugaresLibres}</td>
+                                    <td>{estacionamiento.observaciones}</td>
+                                    <td>
+                                        <div>
+                                            <a className="b_linkMapa" href={estacionamiento.mapa} target="_blank" rel="noreferrer">📍 Ubicación</a>{' '}
 
-
-                        <form onSubmit={this.enviarDatos} >
-                            <div className="form-group">
-                                <label htmlFor="">Nombre:</label>
-                                <input type="text" readOnly name="nombrePlayaDeEstacionamiento" id="nombrePlayaDeEstacionamiento" onChange={this.cambioValor} value={estacionamiento.nombrePlayaDeEstacionamiento} className="form-control" placeholder="" aria-describedby="helpId" />
-
-                            </div>
-
-                            <div className="form-group">
-                                <label htmlFor="">Ubicacion:</label>
-                                <input type="text" readOnly name="ubicacion" id="ubicacion" onChange={this.cambioValor} value={estacionamiento.ubicacion} className="form-control" placeholder="" aria-describedby="helpId" />
-
-                            </div>
-
-                            <div className="form-group">
-                                <label htmlFor="">Lugares Libres:</label>
-                                <input type="text" readOnly name="lugaresLibres" id="lugaresLibres" onChange={this.cambioValor} value={estacionamiento.lugaresLibres} className="form-control" placeholder="" aria-describedby="helpId" />
-
-                            </div>
-
-                            <div className="form-group">
-                                <label htmlFor="">Observaciones:</label>
-                                <input type="text" readOnly name="observaciones" id="observaciones" onChange={this.cambioValor} value={estacionamiento.observaciones} className="form-control" placeholder="" aria-describedby="helpId" />
-
-                            </div>
-
-                            <br></br>
-                            <div>
-                                <a className="b_linkMapa" href={estacionamiento.mapa} target="_blank" rel="noreferrer">Ver Mapa</a>{' '}
-
-                                <button  className="b_estacionar"
-                                    onClick={() => this.estacionar(estacionamiento.idPlayaDeEstacionamiento, this.props.usuario.idUsuario)}>Estacionar</button>
-                            </div>
-                            <br></br>
-                            <Link to={"/"} className="btn btn-success">Volver</Link>
-
-
-                        </form>
-                    </div>
-                    <div className="card-footer text-muted">
-
-                    </div>
+                                            <button className="b_estacionar"
+                                                onClick={() => this.estacionar(estacionamiento.idPlayaDeEstacionamiento, this.props.usuario.idUsuario)}>✔️Estacionar</button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <Link to={"/"} className="btn btn-success">Volver</Link>
+                    </div>                    
                 </div>
+
+
             );
         }
     }
@@ -148,6 +140,3 @@ class VerEstacionamiento extends React.Component {
 }
 
 export default VerEstacionamiento;
-
-
-
