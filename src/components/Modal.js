cf0842264dc0b0a43387ce375131eecd45b50dc1
Guido Magi <guido_magi@hotmail.com>
2022-06-10 22:04:09 -0300

    Ventana emergente en el login

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -13,9 +13,6 @@ const Modal = ({ isOpen, closeModal, title, mensaje }) => {
                 <div className="modal-content">
                     <div className="modal-header">
                         <h5 className="modal-title">{title}</h5>
-                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
                     </div>
                     <div className="modal-body">
                         <p>{mensaje}</p>
